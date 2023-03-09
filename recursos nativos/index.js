365d3f58ecf37e8b746a9a0348ca0eafa408ddcc
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2023-03-08 22:39:31 -0300

    recursos nativos

---
+++ recursos nativos/index.js
@@ -0,0 +1,16 @@
+let name = "Fernando"
+
+console.log(name)
+console.error(name)
+console.warn(name)
+
+//window.alert(name)
+
+// name = window.prompt("Insira o seu nome: ")
+
+// let confirmation = window.confirm("Confirma essa ação?")
+// console.log(confirmation)
+
+alert("Hello World")
+prompt("Hello")
+confirm("Confirm?")
