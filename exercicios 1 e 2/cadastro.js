c1ec7eba73c674d578752f4cae6f2c254388596f
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2023-03-14 23:07:40 -0300

    resolucao exercicio cadastro

---
+++ exercicios 1 e 2/cadastro.js
@@ -0,0 +1,10 @@
+const name = window.prompt("Insira o seu primeiro nome: ")
+const surname = window.prompt("Insira o seu sobrenome: ")
+const study = window.prompt("Insira o seu campo de estudo: ")
+const age = window.prompt("Insira o ano de nascimento ")
+
+alert(
+    "\n Nome Completo: " + name + surname +
+    "\n Campo de Estudo: " + study +
+    "\n Idade: " + (2022 - age)
+)
