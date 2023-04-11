3c0dd5c840ac0eb5a8b064c46eabaf0c04a28deb
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2023-04-10 23:50:53 -0300

    continuação while

--- while/while.js
+++ while/while.js
@@ -2,4 +2,12 @@ let velocidade = 80
 
 while(velocidade > 0) {
     alert("O carro está a " + velocidade + "km/h")
+    velocidade -= 20
+    alert("Diminuindo 20 km/h")
+
+    if(velocidade === 40){
+        break
+    }
 }
+
+alert("O carro está na velocidade ideal: " + velocidade + "km/h" )
