569ede48d9342244cf24f454f765e356af5ebbbe
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2023-04-13 23:32:55 -0300

    finalizacao exercicio

--- exercicio 6/cidades.js
+++ exercicio 6/cidades.js
@@ -1,12 +1,18 @@
-const tourist = prompt("Qual é o seu nome?")
-let cities = ""
-let conta = 0
+const turista = prompt("Qual é o seu nome?")
+let cidades = ""
+let contador = 0
 
-let continue = prompt("Você visitou alguma cidade? (S/N)")
+let continuar = prompt("Você visitou alguma cidade? (S/N)")
 
-while (continue === "S") {
-    let city = ("Qual é o nome da cidade que você visitou?")
-    cities += " - " + city + "\n"
-    conta++
-    continue = prompt("Você visitou mais alguma cidade? (S/N)")
+while (continuar === "S") {
+    let cidade = prompt ("Qual é o nome da cidade que você visitou?")
+    cidades += " - " + cidade + "\n"
+    contador++
+    continuar = prompt("Você visitou mais alguma cidade? (S/N)")
 }
+
+alert(
+    "Turista: " + turista + 
+    "\nQuantidade de cidades visitadas: " + contador + 
+    "\nCidades visitadas: \n" + cidades
+)
