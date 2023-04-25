c5c871452826881ce583de6a62b28024899cfa51
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2023-04-24 23:30:52 -0300

    exercicio menu interativo e controle financeiro

--- do while/doWhile.js
+++ do while/doWhile.js
@@ -3,4 +3,6 @@ let velocidade = prompt("Qual a velocidade do veículo?")
 do{
     alert("A velocidade do veículo é de: " + velocidade + "km/h")
     velocidade -= 20
-} while (velocidade >= 0)
+} while (velocidade > 0)
+
+alert("A velocidade do veículo é " + velocidade + "km/h")
